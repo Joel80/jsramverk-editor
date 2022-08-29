c490daf3982107ccf9c192ebedbfb7154a625264
Joel Löfgren <jdtlofgren@gmail.com>
2022-08-29 14:58:44 +0200

    Fixing indents in code

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,25 @@ import Editor from './components/editor/Editor';
 import SaveButton from './components/toolbar/savebutton/Savebutton';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       Real time collaborative text editor
-      </header>
-      <main className="App-main">
-        <div className='App-toolbar'>
-          <SaveButton />
-        </div>
-        <div className='App-editor'>
-          <Editor />
-        </div>
-        
-      </main>
-      <nav className='App-nav'>
+    return (
+        <div className="App">
+          <header className="App-header">
+          Real time collaborative text editor
+          </header>
+          <main className="App-main">
+            <div className='App-toolbar'>
+              <SaveButton />
+            </div>
+            <div className='App-editor'>
+              <Editor />
+            </div>
+            
+          </main>
+          <nav className='App-nav'>
 
-      </nav>
-    </div>
-  );
+          </nav>
+        </div>
+    );
 }
 
 export default App;
