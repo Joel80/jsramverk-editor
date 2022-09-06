ffcf76180ab080b3ef2aae9b71e087ba30553538
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-06 15:19:49 +0200

    Added dropdown component to toolbar

--- src/components/toolbar/toolbar/Toolbar.tsx
+++ src/components/toolbar/toolbar/Toolbar.tsx
@@ -1,10 +1,12 @@
 import SaveButton from '../savebutton/Savebutton'
+import DocDropDown from '../docDropDown/DocDropDown';
 import "./Toolbar.css";
 
-export default function Toolbar () {
+export default function Toolbar({docs}: any) {
 
     return (
         <div className="App-toolbar">
+            <DocDropDown docs = {docs} />
             <SaveButton />
         </div>
     )
