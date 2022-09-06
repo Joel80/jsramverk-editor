d0321161e4e39dea8aecf582e44a68fb5b6e14ed
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-06 15:18:59 +0200

    Added useState, useEffect and function to fetch all documents

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import './App.css';
 import Texteditor from './components/editor/Texteditor';
+import { useState, useEffect } from 'react';
+import docsModel from './models/docs';
 //import SaveButton from './components/toolbar/savebutton/Savebutton';
 import Toolbar from './components/toolbar/toolbar/Toolbar';
 
 function App() {
+    const [docs, setDocs] = useState([]);
+
+    async function fetchDocks() {
+        const allDocs = await docsModel.getAllDocs();
+
+        setDocs(allDocs);
+    }
+
+    useEffect ( () => {
+        (async () => {
+            await fetchDocks();
+        })();
+    }, []);
+
     return (
         <div className="App">
           <header className="App-header">
           Real time collaborative text editor
           </header>
           <main className="App-main">
-              <Toolbar />
+              <Toolbar docs = {docs}/>
               <Texteditor />
           </main>
           <nav className='App-nav'>
