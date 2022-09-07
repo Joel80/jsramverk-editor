043c3389c2cac5e502cc07602dba3185e893c956
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-07 16:23:19 +0200

    Fixed loading of document so that currentDoc and editor content are updated when document is fetched from db

--- src/components/toolbar/docDropDown/DocDropDown.tsx
+++ src/components/toolbar/docDropDown/DocDropDown.tsx
@@ -1,23 +1,34 @@
+import docInterface from '../../../interfaces/doc';
+
 const documentModel = require('../../../models/docs');
 
-export default  function DocDropDown({docs}:any) {
-    
-    async function fetchDoc (e: React.ChangeEvent<HTMLSelectElement> ) {
+export default  function DocDropDown({setDocumentLoaded, docs, setCurrentDoc}: { setDocumentLoaded(param: boolean): void, docs: docInterface[], setCurrentDoc(param: docInterface): void}) {
+
+    async function fetchDoc (e: React.ChangeEvent<HTMLSelectElement>) {
         //console.log(e.target.value);
 
         const id = e.target.value.trim().toString();
 
         console.log(id);
-        const fetchedDoc = await documentModel.getOneDocById(id); 
 
-        console.log(fetchedDoc);
+        if (id !== "-99") {
+            const fetchedDoc = await documentModel.getOneDocById(id);
+            setCurrentDoc(fetchedDoc);
+            setDocumentLoaded(true);
+        } else {
+            setCurrentDoc({_id: null, name:"Noname", html:""});
+            setDocumentLoaded(true);
+        }
+        
+
+        //console.log(fetchedDoc);
 
     }
 
     return (
             <select onChange={(e) => fetchDoc(e)} className="DocList">
-                <option value="-99" key="0">Choose a document</option>
-                {docs.map((doc:any, index:number) => <option value={doc._id} key={index}>{doc.name} - {doc._id}</option>)}
+                <option value="-99" key="0">New document</option>
+                {docs.map((doc: docInterface, index:number) => <option value={doc._id || ""} key={index}>{doc.name} - {doc._id}</option>)}
             </select> 
     );
 };
