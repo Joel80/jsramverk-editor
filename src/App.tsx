3384061f30097f4e9296fe40a1d0d8d46e001c9b
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-07 16:19:16 +0200

    Added state for loading and updating docs

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,49 @@ import { useState, useEffect } from 'react';
 import docsModel from './models/docs';
 //import SaveButton from './components/toolbar/savebutton/Savebutton';
 import Toolbar from './components/toolbar/toolbar/Toolbar';
+import docInterface from './interfaces/doc';
 
 function App() {
-    const [docs, setDocs] = useState([]);
+    let defaultDoc: docInterface = {
+        _id: null,
+        name: "No title",
+        html: "<p>starthtml</p>"
+    };
 
-    async function fetchDocks() {
+    const [docs, setDocs] = useState([]);
+    const [currentDoc, setCurrentDoc] = useState(defaultDoc);
+    const [documentLoaded, setDocumentLoaded] = useState(false);
+
+   console.log(`Log from app: ${currentDoc._id} - ${currentDoc.html} - ${currentDoc.name}`);
+
+   function setEditorContent(content: string) {
+    let element = document.querySelector("trix-editor") as any | null;
+
+        element.value = "";
+        element.editor.setSelectedRange([0, 0]);
+        element.editor.insertHTML(content);
+    }
+
+    async function fetchDocs() {
         const allDocs = await docsModel.getAllDocs();
 
         setDocs(allDocs);
     }
 
+    async function setDoc(doc: docInterface) {
+        await setCurrentDoc(doc);
+    }
+
     useEffect ( () => {
         (async () => {
-            await fetchDocks();
+            await fetchDocs();
         })();
     }, []);
+
+    useEffect (() => {
+        setEditorContent(currentDoc.html);
+        setDocumentLoaded(false);
+    }, [documentLoaded])
 
     return (
         <div className="App">
@@ -26,8 +54,9 @@ function App() {
           Real time collaborative text editor
           </header>
           <main className="App-main">
-              <Toolbar docs = {docs}/>
-              <Texteditor />
+            {/* <div>{currentDoc.html}</div> */}
+              <Toolbar setDocumentLoaded={setDocumentLoaded} setCurrentDoc={setCurrentDoc} docs={docs} currentDoc={currentDoc}/>
+              <Texteditor setCurrentDoc={setCurrentDoc} currentDoc={currentDoc}/>
           </main>
           <nav className='App-nav'>
 
