835e49d785bb6854f068ae7bab19050016330ab3
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-07 16:25:02 +0200

    Changed handleClick function to save document to database

--- src/components/toolbar/savebutton/Savebutton.tsx
+++ src/components/toolbar/savebutton/Savebutton.tsx
@@ -1,7 +1,9 @@
 //import { Editor, TrixEditor } from "react-trix";
 import "./Savebutton.css";
+import docInterface from '../../../interfaces/doc';
+import docModel from '../../../models/docs';
 
-export default  function SaveButton() {
+export default  function SaveButton({currentDoc}: {currentDoc: docInterface}) {
     function handleClick() {
         //console.log("Clicked");
         const editorElement = document.querySelector("trix-editor") as any | null;
@@ -9,6 +11,15 @@ export default  function SaveButton() {
         console.log(`Content as unformatted string:  ${editorElement?.editor.getDocument().toString()}`);
         //Output as html
         console.log(`Content as html-value: ${editorElement.value}`);
+
+        if (currentDoc._id === null ) {
+            //currentDoc.html = editorElement?.editor.getDocument();
+            docModel.saveDoc(currentDoc);
+        } else {
+          //currentDoc.html = editorElement?.editor.getDocument();
+          docModel.updateDoc(currentDoc);
+        }
+
 
       }
 
