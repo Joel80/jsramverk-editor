f32e9ea441808993661245211b0c58f3cb64ccc0
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-07 16:25:58 +0200

    Passing set state functions to Toolbar for passing to children

--- src/components/toolbar/toolbar/Toolbar.tsx
+++ src/components/toolbar/toolbar/Toolbar.tsx
@@ -1,13 +1,16 @@
 import SaveButton from '../savebutton/Savebutton'
 import DocDropDown from '../docDropDown/DocDropDown';
 import "./Toolbar.css";
+import docInterface from '../../../interfaces/doc';
 
-export default function Toolbar({docs}: any) {
+export default function Toolbar(
+    {setDocumentLoaded, docs, setCurrentDoc, currentDoc}: {setDocumentLoaded(param: boolean): void, docs: docInterface[], setCurrentDoc(param: docInterface): void, currentDoc: docInterface}) {
+
 
     return (
         <div className="App-toolbar">
-            <DocDropDown docs = {docs} />
-            <SaveButton />
+            <DocDropDown setDocumentLoaded={setDocumentLoaded} setCurrentDoc={setCurrentDoc} docs = {docs} />
+            <SaveButton currentDoc={currentDoc}/>
         </div>
     )
 }
