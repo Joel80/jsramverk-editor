91bd82968f161a236eed243d7bc2b5098ac89be4
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-08 08:54:14 +0200

    Removed id from dropdown display, changed name of new document to No title, code formatting

--- src/components/toolbar/docDropDown/DocDropDown.tsx
+++ src/components/toolbar/docDropDown/DocDropDown.tsx
@@ -2,33 +2,34 @@ import docInterface from '../../../interfaces/doc';
 
 const documentModel = require('../../../models/docs');
 
-export default  function DocDropDown({setDocumentLoaded, docs, setCurrentDoc}: { setDocumentLoaded(param: boolean): void, docs: docInterface[], setCurrentDoc(param: docInterface): void}) {
+export default  function DocDropDown({setDocumentLoaded, docs, setCurrentDoc}: {setDocumentLoaded(param: boolean): void, docs: docInterface[], setCurrentDoc(param: docInterface): void}) {
 
     async function fetchDoc (e: React.ChangeEvent<HTMLSelectElement>) {
         //console.log(e.target.value);
 
         const id = e.target.value.trim().toString();
 
-        console.log(id);
+        //console.log(id);
 
         if (id !== "-99") {
             const fetchedDoc = await documentModel.getOneDocById(id);
             setCurrentDoc(fetchedDoc);
             setDocumentLoaded(true);
         } else {
-            setCurrentDoc({_id: null, name:"Noname", html:""});
+            setCurrentDoc({_id: null, name:"No title", html:""});
             setDocumentLoaded(true);
         }
         
 
         //console.log(fetchedDoc);
+        //console.log(`Log from dropdown: ${currentDoc.name}`);
 
     }
 
     return (
-            <select onChange={(e) => fetchDoc(e)} className="DocList">
-                <option value="-99" key="0">New document</option>
-                {docs.map((doc: docInterface, index:number) => <option value={doc._id || ""} key={index}>{doc.name} - {doc._id}</option>)}
+            <select id="documentSelect" onChange={(e) => fetchDoc(e)} className="DocList">
+                <option value= "-99" key="0">New document</option>
+                {docs.map((doc: docInterface, index:number) => <option value={doc._id || ""} key={index}>{doc.name} {/* - {doc._id} */}</option>)}
             </select> 
     );
 };
