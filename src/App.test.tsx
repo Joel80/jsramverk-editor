4a77ac19423d6a4bf5ac7bb91ff1adedcb62c53e
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-13 15:16:07 +0200

    Added first simple test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,42 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import SaveButton from './components/toolbar/savebutton/Savebutton';
+import { unmountComponentAtNode } from "react-dom";
+//import App from './App';
+import Toolbar from './components/toolbar/savebutton/Savebutton';
+/* test('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText("collaborative");
     expect(linkElement).toBeInTheDocument();
+}); */
+
+let container : any = null;
+
+beforeEach(() => {
+    // DOM element as reder target
+    container = document.createElement("div");
+    document.body.appendChild(container);
 });
+
+afterEach(() => {
+    //cleanup in exíting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+})
+
+test('save button renders text Save', () => {
+    let doc = {
+        _id: "",
+        name: "A name",
+        html: ""
+    };
+
+    const mockSetDocumentSaved = jest.fn(()=>{});
+    const mockSetCurrentDock = jest.fn(()=>{});
+
+    render(<SaveButton setDocumentSaved={mockSetDocumentSaved} setCurrentDoc={mockSetCurrentDock} currentDoc={doc}/>);
+    const buttonText = screen.getByText("Save")
+    expect(buttonText).toBeInTheDocument();
+});
+
+
