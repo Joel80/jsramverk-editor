3977f5e374e56b252b9811c58317e9a5dbf11c58
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-18 22:04:51 +0200

    Changing param and argumentlists to use new setState funcs and removing unused

--- src/components/toolbar/toolbar/Toolbar.tsx
+++ src/components/toolbar/toolbar/Toolbar.tsx
@@ -4,13 +4,13 @@ import "./Toolbar.css";
 import docInterface from '../../../interfaces/doc';
 
 export default function Toolbar(
-    {setLoadedDoc, setDocumentSaved, setDocumentLoaded, docs, setCurrentDoc, currentDoc}: {setLoadedDoc(param: docInterface): void, setDocumentSaved(param: boolean): void, setDocumentLoaded(param: boolean): void, docs: docInterface[], setCurrentDoc(param: docInterface): void, currentDoc: docInterface}) {
+    {setLoadedDoc, setSavedDoc, setDocumentSaved, /*  setDocumentLoaded, */ docs, setCurrentDoc, currentDoc}: {setLoadedDoc(param: docInterface): void, setSavedDoc(param: docInterface): void, setDocumentSaved(param: boolean): void, /* setDocumentLoaded(param: boolean): void, */ docs: docInterface[], setCurrentDoc(param: docInterface): void, currentDoc: docInterface}) {
 
 
     return (
         <div className="App-toolbar">
-            <DocDropDown setLoadedDoc={setLoadedDoc} setDocumentLoaded={setDocumentLoaded} setCurrentDoc={setCurrentDoc} docs = {docs} />
-            <SaveButton setCurrentDoc={setCurrentDoc} setDocumentSaved={setDocumentSaved} currentDoc={currentDoc}/>
+            <DocDropDown setLoadedDoc={setLoadedDoc} /* setDocumentLoaded={setDocumentLoaded} */ setCurrentDoc={setCurrentDoc} docs = {docs} />
+            <SaveButton setCurrentDoc={setCurrentDoc} setSavedDoc={setSavedDoc} setDocumentSaved={setDocumentSaved} currentDoc={currentDoc}/>
         </div>
     )
 }
