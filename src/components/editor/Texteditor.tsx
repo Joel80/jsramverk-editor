30b0faef8abc3691c7b47292ed347837a3645531
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-20 15:29:47 +0200

    Handlechange function is now a parameter

--- src/components/editor/Texteditor.tsx
+++ src/components/editor/Texteditor.tsx
@@ -5,36 +5,12 @@ import "./nameForm.css";
 import docInterface from '../../interfaces/doc';
 
 
-export default function Texteditor({ currentDoc, setCurrentDoc }: {currentDoc: docInterface, setCurrentDoc(param: docInterface): void} ) {
-
-    //console.log("Rendering editor");
-
-    //console.log(`Log from editor: ${currentDoc.html}`);
-    //const editorElement = document.querySelector("trix-editor") as any | null;
-    //editorElement?.editor.loadHTML(currentDoc.html);
-
-
-    /* function setEditorContent(content, triggerChange) {
-        let element = document.querySelector("trix-editor");
-  
-        element.value = "";
-        element.editor.setSelectedRange([0, 0]);
-        element.editor.insertHTML(content);
-    } */
-
+export default function Texteditor({ handleChange, currentDoc, setCurrentDoc }: {handleChange(param1: string, param2: string): void, currentDoc: docInterface, setCurrentDoc(param: docInterface): void} ) {
 
     const handleEditorReady = (editor: Editor) => {
         editor.insertString("");
     };
 
-    const handleChange = (html: string, text: string) => {
-        //console.log(html);
-        //console.log(el);
-        let editedDoc = {...currentDoc};
-        editedDoc.html=html;
-        //console.log(currentDoc.html);
-        setCurrentDoc(editedDoc);
-    };
 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         let editedDoc = {...currentDoc};
