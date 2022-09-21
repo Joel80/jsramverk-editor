7c89c336acc8bc469a73ec329a8d57f8f0161060
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-21 12:57:58 +0200

    handleNameChange is now a param

--- src/components/editor/Texteditor.tsx
+++ src/components/editor/Texteditor.tsx
@@ -5,23 +5,23 @@ import "./nameForm.css";
 import docInterface from '../../interfaces/doc';
 
 
-export default function Texteditor({ handleChange, currentDoc, setCurrentDoc }: {handleChange(param1: string, param2: string): void, currentDoc: docInterface, setCurrentDoc(param: docInterface): void} ) {
+export default function Texteditor({ handleChange, handleNameChange, currentDoc, setCurrentDoc }: {handleChange(param1: string, param2: string): void, handleNameChange(param: React.ChangeEvent<HTMLInputElement> ): void, currentDoc: docInterface, setCurrentDoc(param: docInterface): void} ) {
 
     const handleEditorReady = (editor: Editor) => {
         editor.insertString("");
     };
 
 
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    /* const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         let editedDoc = {...currentDoc};
         editedDoc.name = e.target.value;
         setCurrentDoc(editedDoc);
-    } 
+    }  */
   
     return(
       <div data-testid="editor">
-        <input  className="document-name-form" onChange={(e) => handleNameChange(e)} value={currentDoc.name}></input>
-        <TrixEditor value={currentDoc.html} className="trix-content" autoFocus={true} onChange={handleChange} onEditorReady={handleEditorReady} mergeTags={[]} />
+        <input  className="document-name-form" onChange={handleNameChange} value={currentDoc.name}></input>
+        <TrixEditor value={currentDoc.html} className="trix-content" /* autoFocus={true}  */onChange={handleChange} onEditorReady={handleEditorReady} mergeTags={[]} />
       </div>
       
     );
