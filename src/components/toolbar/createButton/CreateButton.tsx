5c50f9acde62e884236debe85fac456d2cc33687
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-22 16:19:20 +0200

    Removed unnecessary parameters

--- src/components/toolbar/createButton/CreateButton.tsx
+++ src/components/toolbar/createButton/CreateButton.tsx
@@ -2,7 +2,7 @@ import "./CreateButton.css";
 import docInterface from '../../../interfaces/doc';
 import docModel from '../../../models/docs';
 
-export default  function CreateButton({handleClick, setCurrentDoc, setSavedDoc, setDocumentSaved, currentDoc}: {handleClick(): void, setCurrentDoc(param: docInterface): void, setSavedDoc(param:docInterface): void, setDocumentSaved(param:boolean): void, currentDoc: docInterface}) {
+export default  function CreateButton({handleClick}: {handleClick(): void}) {
 
     return (
             <button className="create-button" onClick={handleClick}>Create new doc</button> 
