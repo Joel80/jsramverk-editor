4437a912fad7d312c71670089f21fec1ed389707
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-28 11:21:04 +0200

    Added component for login and register

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import authModel from "../../models/auth"
+import UserInterface from '../../interfaces/user';
+
+export default function Login({setToken}: {setToken(param: string): void}) {
+
+    const [user, setUser] = useState({});
+
+    function changeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+        let obj: UserInterface = {};
+
+        obj[event.target.name] = event.target.value;
+
+        setUser({...user, ...obj});
+
+
+    }
+    async function register() {
+        await authModel.register(user);
+        const result = await authModel.login(user);
+
+        if(result.data.token) {
+            setToken(result.data.token);
+        }
+    }
+
+    
+    async function login() {
+       const result = await authModel.login(user);
+
+       if(result.data.token) {
+         setToken(result.data.token);
+       }
+    }
+
+    return(
+        <>
+            <label htmlFor="email">E-mail</label>
+            <input type="email" name="email" onChange={changeHandler}></input>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" onChange={changeHandler}></input>
+
+            <button onClick={register}>Register</button>
+            <button onClick={login}>Log in</button>
+        </>
+    )
+}
