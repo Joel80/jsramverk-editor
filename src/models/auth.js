7abe8b7868946ef6df09dffbf1c32c35b018c871
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-28 10:02:26 +0200

    Added model for auth with functions for register and login

---
+++ src/models/auth.js
@@ -0,0 +1,44 @@
+/**
+ * Model for auth
+ * 
+ */
+
+
+const authModel = {
+    baseUrl: window.location.href.includes("localhost") ? 
+        "http://localhost:1337" :
+        "https://jsramverk-editor-jolf20.azurewebsites.net",
+
+    register: async function register(user) {
+        const response = await fetch(`${authModel.baseUrl}/auth/register`, {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            },
+
+        });
+
+        const result = await response.json();
+    },
+
+    login: async function login(user) {
+        const response = await fetch(`${authModel.baseUrl}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            },
+
+        });
+
+        const result = await response.json();
+
+        return result;
+    },
+
+    
+
+}
+
+export default authModel;
