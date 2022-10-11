0f2bdfc82a16054c7f3c844a79be6abdf0f523e9
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-11 15:04:51 +0200

    Added new param and arg for saving to pdf

--- src/components/toolbar/toolbar/Toolbar.tsx
+++ src/components/toolbar/toolbar/Toolbar.tsx
@@ -2,11 +2,12 @@
 import CreateButton from '../createButton/CreateButton'
 import DocDropDown from '../docDropDown/DocDropDown';
 import ShareForm from '../shareForm/ShareForm';
+import PdfGenerator from '../pdfGenerator/PdfGenerator'
 import "./Toolbar.css";
 import docInterface from '../../../interfaces/doc';
 
 export default function Toolbar(
-    {handleClick, setLoadedDoc, docs, setCurrentDoc, currentDoc, token, setUsers, users}: {
+    {handleClick, setLoadedDoc, docs, setCurrentDoc, currentDoc, token, setUsers, users, createPdf}: {
         handleClick(): void, 
         setLoadedDoc(param: docInterface): void, 
         setSavedDoc(param: docInterface): void, 
@@ -16,7 +17,8 @@ export default function Toolbar(
         currentDoc: docInterface,
         token: string,
         setUsers(param: []): void,
-        users: string[]
+        users: string[],
+        createPdf(): void
     }
     ){
 
@@ -25,7 +27,11 @@ export default function Toolbar(
             <DocDropDown setLoadedDoc={setLoadedDoc} setCurrentDoc={setCurrentDoc} docs = {docs} token={token} setUsers={setUsers} />
             <CreateButton handleClick={handleClick} />
             {currentDoc._id?
-                <ShareForm setCurrentDoc={setCurrentDoc} currentDoc={currentDoc} setUsers={setUsers} users={users}/>
+                <>
+                    <ShareForm setCurrentDoc={setCurrentDoc} currentDoc={currentDoc} setUsers={setUsers} users={users}/>
+                    <PdfGenerator createPdf={createPdf} />
+                </>
+                
                 :
                 <></>
             }
