8737dcb4c16af33dbc39d209e99b8b47c22ef7b5
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-13 14:23:52 +0200

    Component for code console

---
+++ src/components/editor/CodeConsole.tsx
@@ -0,0 +1,11 @@
+import './codeConsole.css'
+
+export default function CodeConsole() {
+
+    return (
+        <div id="codeConsole" className="code-console">
+            Click run code to see the result of your efforts here...
+        </div>
+
+    );
+}
