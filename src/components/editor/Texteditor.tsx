a59b75005b6b38c781ef2671f02c2b5b633e01a8
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-14 14:42:22 +0200

    Setting comment as Trix.config textAttribute

--- src/components/editor/Texteditor.tsx
+++ src/components/editor/Texteditor.tsx
@@ -2,12 +2,24 @@ import "trix/dist/trix";
 import { Editor, TrixEditor } from "react-trix";
 import "./trix.css";
 import docInterface from '../../interfaces/doc';
+/* @ts-ignore */
+import Trix from "trix"
+
 
 export default function Texteditor({ handleChange, currentDoc }: {handleChange(param1: string, param2: string): void, currentDoc: docInterface} ) {
 
+    Trix.config.textAttributes.comment = { 
+      tagName: "comment",
+      style: {
+        backgroundColor: "purple"
+      },
+    }
+    
     const handleEditorReady = (editor: Editor) => {
         editor.insertString("");
     };
+
+  
 
 
     /* const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,4 +34,4 @@ export default function Texteditor({ handleChange, currentDoc }: {handleChange(p
       </div>
       
     );
-  }
+}
