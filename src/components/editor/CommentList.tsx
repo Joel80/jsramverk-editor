d165736ea88c04680e0515dca40b008ed78e52db
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-14 16:19:59 +0200

    Component for displayong list of comment cards

---
+++ src/components/editor/CommentList.tsx
@@ -0,0 +1,18 @@
+import CommentCard from './CommentCard';
+import docComment from '../../interfaces/comment';
+
+export default function CommentList({comments}: {comments: docComment[]}) {
+    const commentItems = comments.map((comment: docComment, index) => {
+        if(comment.user && comment.date && comment.range && comment.text)
+        return <CommentCard
+                        comment={comment}
+                        key={index}
+                />
+    });
+
+    return (
+        <div className="comment-list">
+            {commentItems}
+        </div>
+    )
+}
