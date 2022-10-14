f84480c75443c9b48a8efac84e60b2fb166215a2
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-14 14:43:04 +0200

    Formating

--- src/components/toolbar/docDropDown/DocDropDown.tsx
+++ src/components/toolbar/docDropDown/DocDropDown.tsx
@@ -42,7 +42,6 @@ export default  function DocDropDown({
                     typeChooser.checked=false;
                 }
             }   
-            
             setUsers(docUsers);
             setCurrentDoc(fetchedDoc);
             setLoadedDoc(fetchedDoc);
