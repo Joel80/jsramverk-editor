8ce28bd39e0265d8b4d739815f52419e08c0bb94
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-14 14:46:55 +0200

    Added array of comments as prop on doc interface

--- src/interfaces/doc.ts
+++ src/interfaces/doc.ts
@@ -1,9 +1,9 @@
-import comment from './comment';
+import docComment from './comment';
 export default interface doc {
     _id: string | null;
     name: string;
     html: string | undefined;
     allowed_users: string[];
     code: boolean | undefined;
-    comments: comment[];
+    comments: docComment[];
 }
