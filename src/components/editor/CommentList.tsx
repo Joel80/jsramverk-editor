b228ae6cd214a2b7478d64264581263331ab690f
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-15 12:18:18 +0200

    Passing an id to each comment card

--- src/components/editor/CommentList.tsx
+++ src/components/editor/CommentList.tsx
@@ -2,12 +2,13 @@ import CommentCard from './CommentCard';
 import docComment from '../../interfaces/comment';
 
 export default function CommentList({comments}: {comments: docComment[]}) {
+
     const commentItems = comments.map((comment: docComment, index) => {
-        if(comment.user && comment.date && comment.range && comment.text)
-        return <CommentCard
-                        comment={comment}
-                        key={index}
-                />
+        if(comment.user && comment.date && comment.range && comment.text) {
+            return <CommentCard _id={`commentCard${index}`} comment={comment} key={index} />
+        }
+
+        return <></>;
     });
 
     return (
