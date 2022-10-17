f83c220bac5dfd4593f080af3bac49df142ca914
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-17 17:27:59 +0200

    New version of type chooser where change function is incorporated in typechooser

--- src/components/toolbar/typeChooser/TypeChooser.tsx
+++ src/components/toolbar/typeChooser/TypeChooser.tsx
@@ -1,4 +1,16 @@
-export default function TypeChooser({handleModeChange}: {handleModeChange(): void}) {
+import { MutableRefObject } from "react";
+import docInterface from "../../../interfaces/doc";
+
+export default function TypeChooser({codeMode, setCurrentDoc, setLoadedDoc}: {codeMode: MutableRefObject<boolean>, setCurrentDoc(param: docInterface): void, setLoadedDoc(param: docInterface): void}) {
+
+    function handleModeChange() {
+        codeMode.current = !codeMode.current;
+        let doc = {_id: null, name:"No title", html:"", allowed_users: [], code: codeMode.current, comments: []}
+        setCurrentDoc(doc);
+        setLoadedDoc(doc);
+
+        console.log(codeMode.current);
+    }
 
     return (
             <>
