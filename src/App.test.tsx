594786121986571c96ec552489ce8ced6f24a259
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-20 15:54:25 +0200

    Added test to check that message is displayed in app and model called with correct args when user is added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,7 +10,7 @@ jest.mock('socket.io-client');
 
 
 // Adding new user tests
- test('Adding a new user calls correct functions with correct arguments', async () => {
+ test('Adding a new user calls passed in and state functions with correct arguments', async () => {
     docsModel.emailNewUser = jest.fn();
 
     const mockSetCurrentDoc = jest.fn();
@@ -51,6 +51,59 @@ jest.mock('socket.io-client');
     expect(mockSetCurrentDoc).toHaveBeenCalledTimes(1);
 
  });
+
+ test('adding a user shows message in app and calls model with correct arguments', async() => {
+    render(<App />);
+
+    authModel.login = jest.fn().mockReturnValue({data: {token: "1", email:"test@test.se"}});
+
+    docsModel.emailNewUser = jest.fn();
+
+    const doc = {
+        _id: "1",
+        name: "A name",
+        html: "Some html",
+        allowed_users: ["test@test.se"],
+        code: false,
+        comments: []
+    }
+    
+    docsModel.getOneDocById = jest.fn().mockReturnValue(doc);
+
+    docsModel.getUsers = jest.fn().mockReturnValue(["test@test.se"]);
+
+    docsModel.getAllDocs = jest.fn().mockReturnValue(
+        [ doc ] 
+    );
+
+    docsModel.saveDocQL = jest.fn().mockReturnValue("1");
+
+    const logInButton = screen.getByText("Log in");
+
+    const user = userEvent.setup();
+
+    await user.click(logInButton);
+
+    const createDocButton = screen.getByText("Create new doc");
+
+    await user.click(createDocButton);
+
+    const button = screen.getByText('Share');
+
+    const input = screen.getByTestId('add-user-input');
+
+    await user.type(input, "test2@test.se");
+
+    await user.click(button);
+
+    expect(docsModel.emailNewUser).toHaveBeenCalledWith("test@test.se", 'test2@test.se', "1");
+
+    const message = screen.getByText("User added!");
+
+    expect(message).toBeInTheDocument();
+
+});
+
 
 // Type chooser / code mode test
  test('Clicking on type chooser then on create new doc renders code editor', async () => {
